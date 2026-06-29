625d987cdbf6f8be345cc154c15c9d9926238da6
bizi.work <bizi.mod@gmail.com>
2026-06-29 12:49:23 +0300

    Add market/models for item and artifact, add update prices

--- market/models.py
+++ market/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from resources.models import Resource
-
+from crafting.models import Artifact,Item
 
 class MarketPrice(models.Model):
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE, related_name='market_price')
@@ -11,7 +11,59 @@ class MarketPrice(models.Model):
     updated_at = models.DateTimeField()
 
     class Meta:
-        unique_together = ('resource', 'city')
+        verbose_name = 'Resource Market Price'
+        verbose_name_plural = 'Resource Market Prices'
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=['resource', 'city'],
+                name='unique_together_resource_and_city'
+            )
+        ]
 
     def __str__(self):
         return f'{self.resource} - {self.city}: sell {self.sell_price_min}, buy {self.buy_price_max}'
+
+class ItemMarketPrice(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='market_prices')
+    city = models.CharField(max_length=50)
+    sell_price_min = models.PositiveIntegerField(default=0)
+    buy_price_max = models.PositiveIntegerField(default=0)
+
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Item Market Price'
+        verbose_name_plural = 'Item Market Prices'
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=['item', 'city'],
+                name='unique_together_item_and_city'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.item} - {self.city}: sell {self.sell_price_min}, buy {self.buy_price_max}'
+
+class ArtifactMarketPrice(models.Model):
+    artifact = models.ForeignKey(Artifact, on_delete=models.CASCADE, related_name='market_prices')
+    city = models.CharField(max_length=50)
+    sell_price_min = models.PositiveIntegerField(default=0)
+    buy_price_max = models.PositiveIntegerField(default=0)
+
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        verbose_name = 'Artifact Market Price'
+        verbose_name_plural = 'Artifact Market Prices'
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=['artifact', 'city'],
+                name='unique_together_artifact_and_city'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.artifact} - {self.city}: sell {self.sell_price_min}, buy {self.buy_price_max}'
--- market/services.py
+++ market/services.py
@@ -1,8 +1,9 @@
 import requests
 from django.utils.dateparse import parse_datetime
 
-from market.models import MarketPrice
+from market.models import MarketPrice, ItemMarketPrice, ArtifactMarketPrice
 from resources.models import Resource
+from crafting.models import Artifact, Item
 
 
 class AlbionMarketService:
@@ -20,7 +21,7 @@ class AlbionMarketService:
         return response.json()
 
     @classmethod
-    def _update_prices_by_item_ids(cls, item_ids):
+    def _update_prices_by_item_ids(cls, item_ids, source_model, price_model, fk_field):
         if not item_ids:
             return
         api_data = cls.fetch_prices(item_ids)
@@ -28,21 +29,23 @@ class AlbionMarketService:
         for item_data in api_data:
             item_id = item_data['item_id']
             try:
-                resource = Resource.objects.get(item_id=item_id)
-            except Resource.DoesNotExist:
+                obj = source_model.objects.get(item_id=item_id)
+            except source_model.DoesNotExist:
                 continue
 
             update_at = (parse_datetime(item_data.get('sell_price_min_date')) or parse_datetime(
                 item_data.get('buy_price_max_date')))
 
-            MarketPrice.objects.update_or_create(
-                resource=resource,
-                city=item_data['city'],
+            price_model.objects.update_or_create(
+                **{
+                    fk_field: obj,
+                    "city": item_data["city"],
+                },
                 defaults={
-                    'sell_price_min': item_data['sell_price_min'] or 0,
-                    'buy_price_max': item_data['buy_price_max'] or 0,
-                    'updated_at': parse_datetime(item_data['sell_price_min_date'])
-                }
+                    "sell_price_min": item_data["sell_price_min"] or 0,
+                    "buy_price_max": item_data["buy_price_max"] or 0,
+                    "update_at": update_at,
+                },
             )
 
     @classmethod
@@ -50,10 +53,45 @@ class AlbionMarketService:
         # обновляет все ресурсы в базе данных
         resources = Resource.objects.exclude(item_id__isnull=True)
         item_ids = list(resources.values_list('item_id', flat=True))
-        cls._update_prices_by_item_ids(item_ids)
+        cls._update_prices_by_item_ids(
+            item_ids=item_ids,
+            source_model=Resource,
+            price_model=MarketPrice,
+            fk_field='resource',
+        )
 
     @classmethod
-    def update_prices_for_resources(cls,resources):
-        # обновляет ресурсы учавствующие в переработке
+    def update_item_prices(cls):
+        items = Item.objects.exclude(item_id__isnull=True)
+        item_ids = list(items.values_list("item_id", flat=True))
+
+        cls._update_prices_by_item_ids(
+            item_ids=item_ids,
+            source_model=Item,
+            price_model=ItemMarketPrice,
+            fk_field="item",
+        )
+
+    @classmethod
+    def update_artifact_prices(cls):
+        artifacts = Artifact.objects.exclude(item_id__isnull=True)
+        item_ids = list(artifacts.values_list("item_id", flat=True))
+
+        cls._update_prices_by_item_ids(
+            item_ids=item_ids,
+            source_model=Artifact,
+            price_model=ArtifactMarketPrice,
+            fk_field="artifact",
+        )
+
+    @classmethod
+    def update_prices_for_resources(cls, resources):
+        # Обновляет цены только для ресурсов, участвующих в переработке
         item_ids = [resource.item_id for resource in resources if resource.item_id]
-        cls._update_prices_by_item_ids(item_ids)
+
+        cls._update_prices_by_item_ids(
+            item_ids=item_ids,
+            source_model=Resource,
+            price_model=MarketPrice,
+            fk_field='resource',
+        )
